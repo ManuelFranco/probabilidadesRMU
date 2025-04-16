ad8743634640d86a081c39327befd7d3067ead7f
ManuelFranco <manuel.francop@um.es>
2025-04-16 10:24:59 +0200

    Path fix

--- server/index.js
+++ server/index.js
@@ -14,11 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-
 app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente');
 });
